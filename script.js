312a833be580e8f379db52ccb45310abcfa8937f
Muhammad Taufiq <56469414+MuhammadTaufiq01@users.noreply.github.com>
2020-12-13 22:59:43 +0700

    -

---
+++ script.js
@@ -0,0 +1,28 @@
+// var x = 12;
+// console.log('Hello World');
+// console.log('isi dari variable x adalah' + 10);
+
+// alert ('halo');
+// alert ('nama');
+// alert ('saya');
+// alert ('taufiq');
+
+// var nama = prompt ('masukkan nama:');
+// alert (nama);
+
+// var tes = confirm ('kamu yakin?');
+// alert (tes);
+// if (tes === true) {
+//     alert ('user menekan OK!');
+// } else {
+//     alert ('user menekan cancel');
+// }
+
+alert ('selamat datang');
+var lagi = true;
+while (lagi === true) {
+    var nama = prompt ('masukkan nama');
+    alert ('halo ' + nama);
+    lagi = confirm ('coba lagi?');
+}
+alert ('terimakasih');
