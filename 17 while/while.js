17a5e50d1a97c2fa103769e8b39757c6abf3f240
Muhammad Taufiq <56469414+MuhammadTaufiq01@users.noreply.github.com>
2020-12-15 18:11:48 +0700

    -

--- 17 while/while.js
+++ 17 while/while.js
@@ -2,8 +2,14 @@
 //     console.log ('world');
 // }
 
-var ulang = true;
-while (ulang) {
-    console.log('Hello World');
-    ulang = confirm ('lagi?');
-}
+// var ulang = true;
+// while (ulang) {
+//     console.log('Hello World');
+//     ulang = confirm ('lagi?');
+// }
+
+// var nilaiAwal = 1;
+// while (nilaiAwal <= 10) {
+//     console.log('Hello World ' + nilaiAwal + 'x');
+//     nilaiAwal++;
+// }
