8a04e5577ddd156adc99b62677061a9df26c9913
Muhammad Taufiq <56469414+MuhammadTaufiq01@users.noreply.github.com>
2020-12-27 10:11:19 +0700

    -

---
+++ 20 for/angkot.js
@@ -0,0 +1,10 @@
+var jumlahAngkot = 10, angkotBeroperasi = 6, noAngkot = 1;
+
+while (noAngkot <= angkotBeroperasi) {
+    console.log ('Angkot ' + noAngkot + ' Beroperasi dengan baik');
+    noAngkot++;
+}
+
+for (noAngkot; noAngkot <= jumlahAngkot; noAngkot++){
+    console.log ('Angkot ' + noAngkot + ' Tidak beroperasi');
+}
